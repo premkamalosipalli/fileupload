b96ac42416d5bf5ebccb96efd182b76e7d7c8124
premosipalli <po@veritypay.com>
2023-10-30 15:58:38 -0500

    fix(core): updated api data

--- src/services/loginService/loginApi.ts
+++ src/services/loginService/loginApi.ts
@@ -2,20 +2,13 @@ import { request } from '@umijs/max';
 
 // Define the base URL for your backend API
 const backendApiBaseUrl = 'http://localhost:8080';
-export async function loginLog(params: API.UserLoginBodyDataType) {
+export async function loginLog(data?: any) {
   // @ts-ignore
   return request<API.ResponseDataType>(
     `${backendApiBaseUrl}/api/v1/user/login`,
     {
       method: 'POST',
-      params: params,
+      data: data,
     },
-  )
-    .then((response: any) => {
-      return response;
-    })
-    .catch(function (error: any) {
-      // console.log('api/v1/user/login catch error', error);
-      return error;
-    });
+  );
 }
--- src/services/loginService/loginService.ts
+++ src/services/loginService/loginService.ts
@@ -1,5 +1,5 @@
 import { loginLog } from './loginApi';
 
-export async function userLoginService(params: API.UserLoginBodyDataType) {
-  return await loginLog(params);
+export async function userLoginService(data?: any) {
+  return await loginLog(data);
 }
