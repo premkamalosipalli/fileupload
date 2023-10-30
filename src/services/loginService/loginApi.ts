a8634d5358d8c0586385c42e1d7d5242c49fa1cc
premosipalli <po@veritypay.com>
2023-10-30 15:51:40 -0500

    chore(release): removed unused files

--- src/services/loginService/loginApi.ts
+++ src/services/loginService/loginApi.ts
@@ -1,11 +1,16 @@
 import { request } from '@umijs/max';
 
+// Define the base URL for your backend API
+const backendApiBaseUrl = 'http://localhost:8080';
 export async function loginLog(params: API.UserLoginBodyDataType) {
   // @ts-ignore
-  return request<API.ResponseDataType>('/api/v1/user/login', {
-    method: 'POST',
-    params: params,
-  })
+  return request<API.ResponseDataType>(
+    `${backendApiBaseUrl}/api/v1/user/login`,
+    {
+      method: 'POST',
+      params: params,
+    },
+  )
     .then((response: any) => {
       return response;
     })
